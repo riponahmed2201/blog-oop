11223ffd3ad2ad0f787bfd7f86513c877600f155
ripon <riponahmed2201@gmail.com>
2020-12-13 18:10:24 +0600

    blog complete

--- admin/footer.php
+++ admin/footer.php
@@ -5,7 +5,7 @@
     <!--footer start-->
     <footer class="site-footer">
         <div class="text-center">
-            2018 &copy; FlatLab by VectorLab.
+            2018 &copy; by Md. Ripan Mia.
             <a href="#" class="go-top">
                 <i class="fa fa-angle-up"></i>
             </a>
